f3bca18fd05e1fdb58b5bd7630c6644d52ccbb76
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-12-12 20:29:44 +0100

    Aufgabe 4

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -1,4 +1,7 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Aufruf
 {
@@ -6,6 +9,21 @@ public class Aufruf
     {
         Book neu=new Book(200, new Date(),"My Book","1234-1234-1234");
         System.out.println("Das Buch hat " + neu.getPages() + " Seiten, wurde am " + neu.getReleased() + " veröffentlicht und hat den Titel " + neu.getTitle());
+
+
+        List<Book> Liste= new ArrayList<>();
+        Liste.add(new Book(200, new Date(), "Harri Schotter", "0815-0815-0815"));
+        Liste.add(new Book(200, new Date(), "Harris Schotter ist aus", "0815-0815-0816"));
+        Liste.add(new Book(200, new Date(), "Harris Schotter ist wieder da", "0815-0815-0817"));
+        Liste.add(new Book(200, new Date(), "Harri Schotter ohne Schotter", "0815-0815-0818"));
+        Liste.add(new Book(200, new Date(), "Harri Schotter und der Tod durch Schotter", "0815-0815-0819"));
+
+        for(Book a:Liste)
+        {
+            System.out.println(a.getTitle() + " hat " + a.getPages() + " Seiten und folgende ISBN: " + a.getIsbn());
+        }
+
+
 
     }
 
